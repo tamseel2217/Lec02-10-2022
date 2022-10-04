b04d614e908bd22ecf14190533096d37c025a17a
tamseel2217 <93148385+tamseel2217@users.noreply.github.com>
2022-10-04 11:02:12 -0700

    Add files via upload
    Class work 01

---
+++ 02-10-2022-1.dart
@@ -0,0 +1,14 @@
+void main() {
+  int percentage = 63;
+  int newnum = 0;
+  if (percentage >=80){
+    newnum=10;
+    print("$newnum A+");
+  } else if (percentage >=70){
+      newnum=20;
+      print ("$newnum A+");
+  }   else if (percentage >=60){
+        newnum=30;
+        print("$newnum B");
+      }
+}
